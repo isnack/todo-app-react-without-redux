e4032bb6eaffe95dc3046e009ceeaecb184fde7f
isnack <isnack_yagami@hotmail.com>
2020-02-12 05:30:35 -0300

    remove string connection for database

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,6 +1,3 @@
 const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
-module.exports = mongoose.connect(
-  "mongodb+srv://todoapp:todoapp123@todo-elddu.mongodb.net/test?retryWrites=true&w=majority",
-  { useNewUrlParser: true }
-);
+module.exports = mongoose.connect("", { useNewUrlParser: true });
